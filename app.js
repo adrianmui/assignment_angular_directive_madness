67d87d3102680bc250bae5648dfcef4c2b1262ff
Adrian Mui <adrian.mui@gmail.com>
2016-09-23 11:56:48 -0500

    transclude working

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var directiveMadness = angular.module('directiveMadness', []);
+var app = angular.module('directiveMadness', []);
 
+app.controller('formHandler', ["$scope", function($scope) {
+  
+   $scope.check = function () {
+    console.log($scope.rval); 
+   };
+}]);
 
-directiveMadness.directive('mainNav', [function(){
+app.directive('mainNav', [function(){
   return {
     templateUrl: 'main-nav.html',
     restrict: 'E',
     scope: {}
   };
 }]);
+
+app.directive('mainHeader', [function(){
+  return {
+    templateUrl: 'main-header.html',
+    restrict: 'E',
+    transclude: true,
+    scope: {}
+  };
+}]);
+
